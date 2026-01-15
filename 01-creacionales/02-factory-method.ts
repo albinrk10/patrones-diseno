cc712f8da5d3485ede6453aad32dfeead8d18094
albinrk10 <albinrk@gmail.com>
2026-01-14 21:47:24 -0500

    Initial commit

---
+++ 01-creacionales/02-factory-method.ts
@@ -0,0 +1,64 @@
+/**
+ * ! Factory Method:
+ * El patrón Factory Method permite crear objetos sin especificar
+ * la clase exacta del objeto que se creará.
+ *
+ * En lugar de eso, delegamos la creación de objetos a subclases o métodos
+ * que encapsulan esta lógica.
+ *
+ * * Es útil cuando una clase no puede anticipar la clase
+ * * de objetos que debe crear.
+ *
+ * https://refactoring.guru/es/design-patterns/factory-method
+ *
+ */
+
+import { COLORS } from '../helpers/colors.ts';
+
+interface Hamburger {
+    prepare(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cpollo', COLORS.yellow);
+    }
+}
+
+
+class BeefHamburger implements Hamburger {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cres', COLORS.brown);
+    }
+}
+
+abstract class Restaurant {
+    abstract createHamburger(): Hamburger;
+
+    orderHamburger(): void {
+        const hamburger = this.createHamburger();
+        hamburger.prepare();
+    }
+}
+
+class ChickenRestaurant extends Restaurant {
+
+    override createHamburger(): Hamburger {
+        return new ChickenHamburger();
+    }
+
+}
+
+class BeefRestaurant extends Restaurant {
+
+    override createHamburger(): Hamburger {
+        return new BeefHamburger();
+    }
+
+}
+
+function main() {
+
+}
+
+main();
