d22180f5775609048813dfe998c3edf90fb67662
albin <albinrk@gmail.com>
2026-01-15 23:29:54 -0500

    Add  immutability

--- 01-creacionales/05-inmutabilidad.ts
+++ 01-creacionales/05-inmutabilidad.ts
@@ -1,3 +1,6 @@
+
+import { log } from "node:console";
+import { COLORS } from '../helpers/colors.ts';
 /**
  * ! Inmutabilidad con copia
  * Aunque la inmutabilidad es una buena práctica, no siempre es posible.
@@ -6,3 +9,109 @@
  *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
  *
  */
+
+
+class CodeEditorState { 
+    readonly content: string;
+    readonly cursorPosition: number;
+    readonly unsavedChanges: boolean;
+   
+    constructor(
+        content:string,
+        cursorPosition:number,
+         unsavedChanges: boolean
+    ){
+        this.content =content;
+        this.cursorPosition = cursorPosition;
+        this.unsavedChanges = unsavedChanges
+    }
+
+    copyWith({
+        content,
+        cursorPosition,
+        unsavedChanges,
+    }: Partial<CodeEditorState>): CodeEditorState {
+     return new CodeEditorState(
+        content ?? this.content,
+        cursorPosition ?? this.cursorPosition,
+        unsavedChanges ?? this.unsavedChanges
+     )
+    }
+
+    displayState(){
+        console.log('\n%cEstado del editor:', COLORS.green);
+        console.log(`
+            Contenido: ${this.content}
+            Cursor Pos: ${this.cursorPosition}
+            Unsaved changes: ${this.unsavedChanges}
+            `
+        );
+    }
+}
+
+class CodeEditorHistory {
+    private history: CodeEditorState[] =[]
+    private currentIndex: number = -1  //0,1,2,3,4,5,6
+
+    save(state: CodeEditorState):void{
+        if(this.currentIndex < this.history.length -1){
+            this.history = this.history.splice(0,this.currentIndex + 1)
+        }
+        this.history.push(state)
+        this.currentIndex++
+    }
+
+    undo(): CodeEditorState | null {
+        if(this.currentIndex > 0 ){
+            this.currentIndex --;
+            return this.history[this.currentIndex];
+        }
+        return null;
+    }
+
+    redo(): CodeEditorState | null {
+        if(this.currentIndex < this.history.length - 1 ){
+            this.currentIndex++;
+            return this.history[this.currentIndex];
+        }
+
+        return null //1,2,3,4
+    }
+}
+
+
+function main(){
+ const history =new CodeEditorHistory();
+ let editorState =new CodeEditorState("console.log('Hola Mundo');", 2,false);
+ 
+ history.save(editorState);
+
+ console.log('%cEstado inicial',COLORS.blue);
+  editorState.displayState();
+  
+  editorState=editorState.copyWith({
+    content:"console.log('Hola Mundo);\nconsole.log('Nueva linea');",
+    cursorPosition:3,
+    unsavedChanges:  true
+  });
+   history.save(editorState);
+
+   console.log('\n%cDespues de mover el primer cambio',COLORS.blue);
+   editorState.displayState();
+
+   console.log('\n%cDespues de mover el cursor',COLORS.blue);
+   editorState = editorState.copyWith({cursorPosition: 5})
+    history.save(editorState);
+    editorState.displayState();
+
+    console.log('\n%cDespues del Undo',COLORS.blue);
+    editorState = history.undo()!;
+    editorState.displayState();
+
+    console.log('\n%cDespues del Redo',COLORS.blue);
+    editorState = history.redo()!;
+    editorState.displayState();
+
+
+}
+main();
